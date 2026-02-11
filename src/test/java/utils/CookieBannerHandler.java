dec4d095d1a632d05beaf723007996e07467d04c
Sergey Saratov <sergeysaratov@gmail.com>
2026-02-12 01:50:00 +0500

    WIP

--- src/test/java/utils/CookieBannerHandler.java
+++ src/test/java/utils/CookieBannerHandler.java
@@ -28,26 +28,4 @@ public class CookieBannerHandler {
                 .click();
         BANNER.should(Condition.disappear, Duration.ofSeconds(5));
     }
-
-    public static void setConsentCookiesBeforeNavigation() {
-        var cookiesScript =
-                """
-                const domain = window.location.hostname;
-                const expiry = new Date(Date.now() + 365 * 24 * 60 * 60 * 1000).toUTCString();
-                
-                const cookies = [
-                    'fc:consent=granted',
-                    'cookie_consent=accepted',
-                    'gdpr=accepted',
-                    'privacy=accepted',
-                    'consent=true'
-                ];
-                
-                localStorage.setItem('fc:consent', 'granted');
-                localStorage.setItem('fc:showConsentDialog', 'false');
-                localStorage.setItem('cookie_consent', 'accepted');
-                localStorage.setItem('gdpr_consent', 'accepted');
-                """;
-        Selenide.executeJavaScript(cookiesScript);
-    }
 }
